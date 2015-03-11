0beb3c798c6a64b4125c72321da22a2e0f9bf8f6
rtw <rtw1981@gmail.com>
2015-03-11 22:37:32 +1100

    updating to new version teams

--- js/game.js
+++ js/game.js
@@ -1,16 +1,46 @@
 var game = new Phaser.Game(960, 540, Phaser.AUTO, '', { preload: preload, create: create, update: update });
  
-var platforms;
+var platforms,
+	players,
+	stars,
+	bullets;
 
-var score = 0;
-var scoreText;
+var redScoreText,
+	blueScoreText,
+	wintext;
+
 var btn = {
 	right: false,
 	left: false,
 	up: false
 };
-var bulletTime = 0;
-var dir = 1;
+
+
+var	team,
+	player;
+
+var score = {
+		red:0,
+		blue:0
+	},	
+	teams = {
+		red: {
+			numberOfPlayers: 1,
+			players:[]
+		},
+		blue: {
+			numberOfPlayers: 1,
+			players:[]
+		}
+	};
+
+var bulletTime = 0,
+	dir = 1,
+	dx = 0;
+	
+
+	
+	
 
 function preload() {
 	game.load.image('bg', 'assets/bg.png');
@@ -24,7 +54,7 @@ function preload() {
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
 }
- 
+
 function create() {
 
     function readyPlatform() {
@@ -44,19 +74,19 @@ function create() {
 	    ground.body.immovable = true;
 	 
 	    //  Now let's create two ledges
-	    var ledge = platforms.create(300, game.world.height - 200, 'platform');
+	    var ledge = platforms.create(300, game.world.height - 150, 'platform');
 	    ledge.body.immovable = true;
 	 
-	    var ledge = platforms.create(750, game.world.height - 200, 'platform');
+	    var ledge = platforms.create(750, game.world.height - 150, 'platform');
 	 	ledge.body.immovable = true;   
 
-	 	var ledge = platforms.create(1200, game.world.height - 200, 'platform');
+	 	var ledge = platforms.create(1200, game.world.height - 150, 'platform');
 	 	ledge.body.immovable = true;  
 
-	 	var ledge = platforms.create(525, game.world.height - 350, 'platform');
+	 	var ledge = platforms.create(525, game.world.height - 250, 'platform');
 	 	ledge.body.immovable = true;   
 
-	 	var ledge = platforms.create(975, game.world.height - 350, 'platform');
+	 	var ledge = platforms.create(975, game.world.height - 250, 'platform');
 	 	ledge.body.immovable = true;   
     }
 
@@ -64,24 +94,25 @@ function create() {
     	players = game.add.group();
 
     	// The player and its settings
-	    player = players.create(32, game.world.height - 150, 'dude');
+	    player1 = players.create(32, 968, 'dude');
 	 
 	    //  We need to enable physics on the player
-	    game.physics.arcade.enable(player);
+	    game.physics.arcade.enable(player1);
 	 
 	    //  Player physics properties. Give the little guy a slight bounce.
-	    player.body.bounce.y = 0.2;
-	    player.body.gravity.y = 300;
-	    player.body.collideWorldBounds = true;
+	    player1.body.bounce.y = 0.2;
+	    player1.body.gravity.y = 300;
+	    player1.body.collideWorldBounds = true;
 	 
 	    //  Our two animations, walking left and right.
-	    player.animations.add('left', [0, 1, 2, 3], 10, true);
-	    player.animations.add('right', [5, 6, 7, 8], 10, true);
+	    player1.animations.add('left', [0, 1, 2, 3], 10, true);
+	    player1.animations.add('right', [5, 6, 7, 8], 10, true);
 
-	    player.body.gravity.y = 300;
+	    player1.body.gravity.y = 300;
+	    player1.team = 'red';
 
     	// The player and its settings
-	    player2 = players.create(game.world.width - 150, game.world.height - 150, 'dude');
+	    player2 = players.create(1770, 968, 'dude');
 	 
 	    //  We need to enable physics on the player
 	    game.physics.arcade.enable(player2);
@@ -96,78 +127,105 @@ function create() {
 	    player2.animations.add('right', [5, 6, 7, 8], 10, true);
 
 	    player2.body.gravity.y = 300;
-    }
+	    player2.team = 'blue';
 
-    function readyStars() {
-    	stars = game.add.group();
- 
-    	stars.enableBody = true;
-	 
-	    //  Here we'll create 12 of them evenly spaced apart
-	    for (var i = 0; i < 12; i++)
-	    {
-	        //  Create a star inside of the 'stars' group
-	        var star = stars.create(i * 70, 0, 'star');
-	 
-	        //  Let gravity do its thing
-	        star.body.gravity.y = 6;
-	 
-	        //  This just gives each star a slightly random bounce value
-	        star.body.bounce.y = 0.7 + Math.random() * 0.2;
-	    }
+	    teams.red.players.push(player1);
+	    teams.blue.players.push(player2);
     }
 
     function readyGameController() {
     	if (!game.device.desktop) {
-	 	
-	
-
-			buttonright = game.add.button(200, 400, 'btngreen', function(){});  
+	 		var buttonright = game.add.button(125, 200, 'btngreen', function(){});  
 	    	buttonright.events.onInputDown.add(function(){btn.right=true});
 	    	buttonright.events.onInputUp.add(function(){btn.right=false});
+	    	buttonright.fixedToCamera = true;
 
-	    	buttonleft = game.add.button(0, 400, 'btngreen', function(){});  
+	    	var buttonleft = game.add.button(25, 200, 'btngreen', function(){});  
 	    	buttonleft.events.onInputDown.add(function(){btn.left=true});
 	    	buttonleft.events.onInputUp.add(function(){btn.left=false});
+	    	buttonleft.fixedToCamera = true;
 
-	    	buttonup = game.add.button(100, 300, 'btngreen', function(){});  
+	    	var buttonup = game.add.button(875, 125, 'btngreen', function(){});  
 	    	buttonup.events.onInputDown.add(function(){btn.up=true});
 	    	buttonup.events.onInputUp.add(function(){btn.up=false});
+	    	buttonup.fixedToCamera = true;
 
-	    	buttondown = game.add.button(100, 500, 'btngreen', function(){});  
-	    	buttondown.events.onInputDown.add(function(){btn.down=true});
-	    	buttondown.events.onInputUp.add(function(){btn.down=false});
+	    	var buttonshoot = game.add.button(875, 225, 'btngreen', function(){});  
+	    	buttonshoot.events.onInputDown.add(function(){btn.shoot=true});
+	    	buttonshoot.events.onInputUp.add(function(){btn.shoot=false});
+	    	buttonshoot.fixedToCamera = true;
     	}
+    }
+
+    function readyWinText() {
+    	wintext = game.add.text(500, 250, '');
+	    wintext.anchor.set(0.5);
+	    wintext.align = 'center';
+
+	    wintext.font = 'Arial Black';
+	    wintext.fontSize = 70;
+	    wintext.fontWeight = 'bold';
+
+	    wintext.setShadow(0, 0, 'rgba(0, 0, 0, 0.5)', 0);
+	    wintext.fixedToCamera = true;
+    }
+
+    function readyScoreText() {
+    	redScoreText = game.add.text(16, 16, 'Red: 0', { fontSize: '32px', fill: '#FFF' });
+	    redScoreText.fixedToCamera = true;
+
+	    blueScoreText = game.add.text(830, 16, 'Blue: 0', { fontSize: '32px', fill: '#FFF' });
+	    blueScoreText.fixedToCamera = true;
     }
 
     //  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+	game.world.setBounds(0, 0, 1920, 1080);
+
     //  A simple background for our game
     game.add.sprite(0, 0, 'bg');
+
 
 	// Setup the keyboard for input 
     cursors = game.input.keyboard.createCursorKeys();
     fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
-    //game.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
-	game.world.setBounds(0, 0, 1920, 1080);
-
     readyPlatform();
 
-    readyPlayers();
-
-    readyStars();
-
 	readyGameController();
+
+	stars = game.add.group();
+   	stars.enableBody = true;
 
 	bullets = game.add.group();
 	bullets.enableBody = true;
 
-	scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#FFF' });
-    scoreText.fixedToCamera = true;
+	readyScoreText();
+
+    readyWinText();
+
+
+    var teamname = getParameterByName('team') || 'red';
+    team = teams[teamname];
+
+	teams.red.numberOfPlayers = getParameterByName('red') || 1;
+	teams.blue.numberOfPlayers = getParameterByName('blue') || 1;
+	readyPlayers();
+
+	var playernumber = getParameterByName('player') || 0;
+
+	player = team.players[playernumber];
 
 	game.camera.follow(player);
+
+
+
+	var server = getParameterByName('server');
+	var gameid = getParameterByName('gameid');
+
+	Client.start(server, gameid);
 }
  
 function update() {
@@ -176,39 +234,29 @@ function update() {
 	    star.kill();
 
 	    //  Add and update the score
-    	score += 10;
-    	scoreText.text = 'Score: ' + score;
+	    if (player.team == 'red' && team == 'red') {
+	    	score.red += 10;
+	    	Client.updateScore('red', score.red);
+	    	redScoreText.text = 'Red: ' + score.red;
+	    } 
+
+	    if (player.team == 'blue' && team == 'blue') {
+	    	score.blue += 10;
+	    	Client.updateScore('blue', score.blue);
+	    	blueScoreText.text = 'Blue: ' + score.blue;
+	    }
 	}
 
 	function playerHit (player, bullet) {
-        // Removes the star from the screen
-	    player.kill();
-	    bullet.kill();
+		if (player.team == team) return;
 
-	    //  Add and update the score
-    	score += 10;
-    	scoreText.text = 'Score: ' + score;
-	}
+		Client.playerHit(player.team);
 
-	function shoot(player) {
-		if (game.time.now > bulletTime) {
-			
-			if (dir > 0) {
-				bullet = bullets.create(player.x+30, player.y+20, 'bullet');
-				bullet.body.velocity.x = 500;
-			} else {
-				bullet = bullets.create(player.x-2, player.y+20, 'bullet');
-				bullet.body.velocity.x = -500;
-			}
-
-			bulletTime = game.time.now + 100;
-		}
+		hit(player, bullet);
 	}
 
 	//  Collide the player and the stars with the platforms
     game.physics.arcade.collide(players, platforms);
-
-    game.physics.arcade.collide(players, players);
     
     game.physics.arcade.collide(stars, platforms);
 
@@ -227,6 +275,7 @@ function update() {
         player.animations.play('left');
 
         dir = -1;
+        dx = -1;
     }
     else if (cursors.right.isDown || btn.right )
     {
@@ -236,6 +285,7 @@ function update() {
         player.animations.play('right');
 
         dir = 1;
+        dx = 1;
     }
     else
     {
@@ -243,15 +293,98 @@ function update() {
         player.animations.stop();
  
         player.frame = 4;
+		dx = 0;
     }
 
-    if (fireButton.isDown) {
-    	 shoot(player);
+    if (fireButton.isDown || btn.shoot) {
+    	 shoot(player, dir, true);
     }
     
     //  Allow the player to jump if they are touching the ground.
     if ((cursors.up.isDown || btn.up) && player.body.touching.down)
     {
-        player.body.velocity.y = -350;
+        player.body.velocity.y = -250;
     }
+}
+
+
+
+
+
+function readyStars() {
+   	//  Here we'll create 12 of them evenly spaced apart
+    for (var i = 0; i < 27; i++)
+    {
+        //  Create a star inside of the 'stars' group
+        var star = stars.create(i * 70, 500, 'star');
+ 
+        //  Let gravity do its thing
+        star.body.gravity.y = 6;
+ 
+        //  This just gives each star a slightly random bounce value
+        star.body.bounce.y = 0.7 + Math.random() * 0.2;
+    }
+}
+
+function shoot(player, bulletdir, relay) {
+	if (relay && game.time.now < bulletTime) {
+		return;
+	}
+
+	if (bulletdir > 0) {
+		bullet = bullets.create(player.x+30, player.y+20, 'bullet');
+		bullet.body.velocity.x = 500;
+	} else {
+		bullet = bullets.create(player.x-2, player.y+20, 'bullet');
+		bullet.body.velocity.x = -500;
+	}
+
+	bulletTime = game.time.now + 100;
+
+	if (relay) {
+		Client.shoot(team, bulletdir);
+	}
+}
+
+function hit(player, bullet) {
+	// Removes the star from the screen
+    player.kill();
+
+    if (bullet) {
+    	bullet.kill();
+	}
+
+    //  Add and update the score
+	if (player.team == 'blue' && team == 'red') {
+    	score.red += 50;
+    	Client.updateScore('red', score.red);
+    	redScoreText.text = 'Red: ' + score.red;
+    } 
+
+    if (player.team == 'red' && team == 'blue') {
+    	score.blue += 50;
+    	Client.updateScore('blue', score.blue);
+    	blueScoreText.text = 'Blue: ' + score.blue;
+    }
+
+    var win = false;
+    if (player.team != team) {
+    	if (player.team == 'blue') {
+    		wintext.text = 'Red Wins!';
+    		wintext.fill = '#ff0000';
+    	} else {
+    		wintext.text = 'Blue Wins!'
+    		wintext.fill = '#0000ff';
+    	}
+    } else {
+    	if (player.team == 'red') {
+    		wintext.text = 'Blue Wins!';
+    		wintext.fill = '#0000ff';
+    	} else {
+    		wintext.text = 'Red Wins!';
+    		wintext.fill = '#ff0000';
+    	}
+    }
+
+    game.paused = true;
 }
--- server/server.js
+++ server/server.js
@@ -1,4 +1,40 @@
-var ws = require("nodejs-websocket")
+var ws = require("nodejs-websocket"),
+    Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key: 'zKfdnWS1Ym2ooZWi54k0Dvl7P',
+  consumer_secret: 'Khn10wdb9qMzqxtyHChdEvfIgoBaMmvYnkxxiK2tn4bzwM9uAN',
+  access_token_key: '16461402-Vjf4dv2AShYr7QOe2nEDXRSCD1oEj1XjpBI8dgP14',
+  access_token_secret: '4m3jeLZXZ0azC4gCiaM6LsdNI9U6I3kQI4pR8UE5OFNiB'
+});
+ 
+
+var tweetsrun = '';
+setInterval(function() { 
+    var params = {screen_name: 'nodejs', q: '%23flagger %23game1', result_type:'recent'};
+    client.get('search/tweets', params, function(error, tweets, response){
+      if (!error) {
+        tweets.statuses.forEach(function(item) {
+            if (tweetsrun.indexOf(item.id) >= 0) return;
+
+            var bits = item.text.split(' ');
+
+            var code = bits[bits.length - 1];
+            
+            tweetsrun += item.id;
+            if (code == 'starlight') {
+                server.connections.forEach(function (conn) {
+                    var data = {type:'action', code: 'starlight'};
+                    var msg = JSON.stringify(data);
+                    console.log(msg);
+                    conn.sendText(msg);
+                })
+            }
+        });
+      }
+
+    });
+}, 10000);
 
 var games = [];
 
@@ -8,8 +44,9 @@ var newGame = function(id) {
             name: 'red',
             players: [
                 {
-                    x: 50,
-                    y: 500
+                    x: 32,
+                    y: 968,
+                    dx: 0
                 }
             ]
         },
@@ -17,12 +54,12 @@ var newGame = function(id) {
             name: 'blue',
             players: [
                 {
-                    x: 1150,
-                    y: 500
+                    x: 1770,
+                    y: 968,
+                    dx: 0
                 }
             ]
         },
-        bullets: [],
         id:id
     };
 
@@ -62,17 +99,19 @@ var server = ws.createServer(function (conn) {
             if (msg.team == 'red') {
                 game.redteam.players[0] = {
                     x: msg.x,
-                    y: msg.y
+                    y: msg.y,
+                    dx: msg.dx
                 }
             }
 
             if (msg.team == 'blue') {
                 game.blueteam.players[0] = {
                     x: msg.x,
-                    y: msg.y
+                    y: msg.y,
+                    dx: msg.dx
                 }
             }
-        } else if (msg.type == 'bullet') {
+        } else if (msg.type == 'shoot') {
             var game = findGame(msg.gameid);
             if (!game) return;
 
@@ -80,7 +119,23 @@ var server = ws.createServer(function (conn) {
             server.connections.forEach(function (conn) {
                 conn.sendText(JSON.stringify(msg));
             })
+        } else if (msg.type == 'score') {
+            var game = findGame(msg.gameid);
+            if (!game) return;
+
+            server.connections.forEach(function (conn) {
+                conn.sendText(JSON.stringify(msg));
+            })
+        }else if (msg.type == 'hit') {
+            console.log('hit ' + msg.team);
+            var game = findGame(msg.gameid);
+            if (!game) return;
+
+            server.connections.forEach(function (conn) {
+                conn.sendText(JSON.stringify(msg));
+            })
         }
+
     });
     
     conn.on("close", function (code, reason) {
